090873fe5b357f20d46b40a502e4225be286c394
vinodiWebcode <vinod.kumar@iwebcode.dev>
2022-02-15 17:31:27 +0530

    firebaseAuth

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// App.js
 
-export default function App() {
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from './components/login';
+import Signup from './components/signup';
+import Dashboard from './components/dashboard';
+
+const Stack = createNativeStackNavigator();
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="Signup"
+      screenOptions={{
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#3740FE',
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen
+        name="Signup"
+        component={Signup}
+        options={{ title: 'Signup' }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={
+          { title: 'Login' },
+          { headerLeft: null }
+        }
+      />
+      <Stack.Screen
+        name="Dashboard"
+        component={Dashboard}
+        options={
+          { title: 'Dashboard' },
+          { headerLeft: null }
+        }
+      />
+    </Stack.Navigator>
+    // </NavigationContainer>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  );
+}
---
+++ database/firebase.js
@@ -0,0 +1,17 @@
+// database/firebaseDb.js
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth"
+import "firebase/compat/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCaNztduInYGtXFVDq4NOnHnilHtm7RTQU",
+    authDomain: "rnfirebaseauth-ab8fd.firebaseapp.com",
+    projectId: "rnfirebaseauth-ab8fd",
+    storageBucket: "rnfirebaseauth-ab8fd.appspot.com",
+    messagingSenderId: "747867705745",
+    appId: "1:747867705745:web:edc9ed71abf84d93354a30"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
